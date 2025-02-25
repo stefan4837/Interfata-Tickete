76c1f39d1b79f307cb9775017406553cd9deea9b
stefan4837 <dreamplay879@gmail.com>
2025-02-25 18:55:25 +0200

    Add files via upload

---
+++ tickete/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+author 'stefan4837'
+game 'gta5'
+
+ui_page 'html/index.html' 
+
+files {
+    'html/index.html',
+    'html/script.js',
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
